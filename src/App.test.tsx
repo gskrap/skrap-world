baf250ad8570a39bffd5407035ccb319ff189db5
gskrap <gskrap@gmail.com>
2025-05-10 00:32:13 -0400

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders', () => {
   render(<App />);
   expect(screen.getByText('skrap-world')).toBeInTheDocument();
 });
