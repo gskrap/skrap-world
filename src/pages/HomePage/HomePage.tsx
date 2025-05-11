ff3693e41514eddbe8fcf9280911842d6285a5a6
gskrap <gskrap@gmail.com>
2025-05-11 02:04:47 -0400

    add under construction message

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { FaBomb } from 'react-icons/fa6';
+
 export const HomePage = () => {
   return (
-    <div>home</div>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+      <FaBomb size={240} />
+      <p style={{ marginTop: '16px' }}>[wip: rebuilding the site for 2025]</p>
+    </div>
   );
 }
