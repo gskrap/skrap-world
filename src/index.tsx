5e020e9d6fdaa6e9f345a892470a83f7598dfdf2
gskrap <gskrap@gmail.com>
2025-05-17 14:47:18 -0400

    unregister old service workers

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,14 @@ import App from './App';
 import './styles/reset.css';
 import './styles/index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
