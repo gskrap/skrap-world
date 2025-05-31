19523e147c6c41eca45b4746073460bd94e6aff4
gskrap <gskrap@gmail.com>
2025-05-31 11:22:59 -0400

    test

--- src/pages/MorePage/MorePage.tsx
+++ src/pages/MorePage/MorePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const MorePage = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <p style={{ marginTop: '16px' }}>wip: rebuilding in 2025</p>
+      <p style={{ marginTop: '16px' }}>wip: rebuilding in 2025 ~</p>
     </div>
   );
 };
